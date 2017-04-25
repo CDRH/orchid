a794e475cd56fa4d2ff626c236671c551f868e6d
Greg Tunink <techgique@gmail.com>
2017-04-25 13:51:42 -0500

    Add config prompts; Restore header h2
    Prompt for primary app config variable values
    
    Restore the <h2> in the layout header,
    retrieving its value from config variable "project_subtitle"

--- lib/generators/setup_generator.rb
+++ lib/generators/setup_generator.rb
@@ -37,9 +37,34 @@ class SetupGenerator < Rails::Generators::Base
 
   private
 
+  def config_replace(original, new)
+    gsub_file "#{@new_app}/config/config.yml", original, new
+  end
+
+  def config_set(var_name, value)
+    gsub_file "#{@new_app}/config/config.yml", /^(\s*#{var_name}:).+$/, "\\1 #{value}"
+  end
+
   def copy_configs
     FileUtils.cp("#{@this_app}/lib/generators/templates/config.yml", "#{@new_app}/config/config.example.yml")
     FileUtils.cp("#{@this_app}/lib/generators/templates/config.yml", "#{@new_app}/config/config.yml")
+
+    puts "Please enter the following for initial app customization"
+
+    answer = prompt_for_value("Project Name (Header <h1>)", "Sample Template")
+    config_set("project_name", answer)
+
+    answer = prompt_for_value("Project Short Name (<title>, meta application-name>)", "Template")
+    config_set("project_shortname", answer)
+
+    answer = prompt_for_value("Project Subtitle (Header <h2>)", "Template Subtitle")
+    config_set("project_subtitle", answer)
+
+    answer = prompt_for_value("Dev API Path", "https://cdrhdev1.unl.edu/api")
+    config_replace("api_path: https://cdrhdev1.unl.edu/api", answer)
+
+    answer = prompt_for_value("Production API Path", "https://cdrhapi.unl.edu")
+    config_replace("api_path: https://cdrhapi.unl.edu", answer)
 
     return "Customize your app in config/config.yml".green
   end
@@ -75,6 +100,13 @@ class SetupGenerator < Rails::Generators::Base
   def gitignore
     FileUtils.cp("#{@this_app}/lib/generators/templates/.gitignore", "#{@new_app}/.gitignore")
     return "Add more files to .gitignore which should not be version controlled".green
+  end
+
+  def prompt_for_value(message, default)
+    puts "\n#{message}\n[#{default}]: "
+    value = gets.chomp
+
+    return value.empty? ? default : value
   end
 
   def remove_files
