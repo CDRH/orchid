52ce920e4b271ab656a81a50fb3563ed32ce0e9a
Jessica Dussault <jdussault4@gmail.com>
2017-04-26 14:02:28 -0500

    making sure assets specific to orchid are precompiled

--- lib/orchid/engine.rb
+++ lib/orchid/engine.rb
@@ -1,5 +1,12 @@
 module Orchid
   class Engine < ::Rails::Engine
     isolate_namespace Orchid
+
+    initializer "orchid.assets.precompile" do |app|
+      app.config.assets.precompile += %w(
+        application.js
+        application.scss
+      )
+    end
   end
 end
