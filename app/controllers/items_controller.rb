4953e265f7fe8b3a1f87ea749e76110d09954e8c
Greg Tunink <techgique@gmail.com>
2017-05-08 15:23:19 -0500

    Split Browse into separate pages; Add flash msgs
    Make /browse a nav page based on model's facets, no API request needed
    
    Create /browse/:facet route, controller method, and view
    Reverse facet key URL formatting
    Check if facet key in model facets, else redirect to /browse with flash
    notice
    
    Add flash messages to top of #content-wrapper body contents

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,29 @@
 class ItemsController < ApplicationController
 
   def browse
-    @res = $api.query({"facet_num" => 500, "facet_sort" => "term|asc" }).facets
+  end
+
+  def browse_facet
+    # Reverse facet name from url-formatting
+    @browse_facet = params[:facet]
+    if @browse_facet.include?("-")
+      Facets.facet_info.each_with_index do |(facet_name, facet_info), index|
+        if @browse_facet == helpers.datify_name(facet_name)
+          @browse_facet = facet_name
+          break
+        end
+      end
+    end
+
+    # Get selected facet's info
+    @browse_facet_info = Facets.facet_info[@browse_facet]
+    if @browse_facet_info.blank?
+      redirect_to browse_path, notice: "Cannot browse by key: '#{@browse_facet}'"
+      return
+    end
+
+    # Get facet results
+    @res = $api.query({"facet" => @browse_facet, "facet_num" => 500, "facet_sort" => "term|asc" }).facets
   end
 
   def index
