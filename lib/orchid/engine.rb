59ccebf7f6893b0c4f9f8e8105214436728f6fa7
Greg Tunink <techgique@gmail.com>
2017-09-22 12:54:01 -0500

    Drop unneeded additions to asset precompile list
    These files are covered by the declarations in
    `app/assets/javascripts/orchid.js`

--- lib/orchid/engine.rb
+++ lib/orchid/engine.rb
@@ -1,12 +1,5 @@
 module Orchid
   class Engine < ::Rails::Engine
     isolate_namespace Orchid
-
-    initializer "orchid.assets.precompile" do |app|
-      app.config.assets.precompile += %w(
-        application.js
-        application.scss
-      )
-    end
   end
 end
