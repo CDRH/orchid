95cb7d66c3cd64b3a52cc5e40f891494e3aa9e80
Greg Tunink <techgique@gmail.com>
2017-10-02 17:50:36 -0500

    Add refactored active? helper; Use with main nav
    Add with simpler code and warning if only a regexp passed
    
    Reformat code in _navbar.html.erb to not wrap

--- app/helpers/orchid/application_helper.rb
+++ app/helpers/orchid/application_helper.rb
@@ -1,5 +1,33 @@
 module Orchid::ApplicationHelper
 
+  def active?(value, comparison=nil)
+    if value.class == Regexp
+      if comparison.nil?
+        # Inform if tried to pass only a Regexp to active?
+        raise "current_page? does not support Regexp comparisons"
+      elsif comparison.class == Symbol
+        value.match(params[comparison]) ? "active" : ""
+      else
+        value.match(comparison) ? "active" : ""
+      end
+    else
+      Array(value).each do |v|
+        begin
+          if comparison.nil?
+            return "active" if current_page? v
+          elsif comparison.class == Symbol
+            return "active" if current_page? comparison => v
+          else
+            return "active" if v == comparison
+          end
+        rescue
+          # Rescue when current_page? throws "No route matches" exception
+          return "active" if !comparison.nil? && v == params[comparison]
+        end
+      end
+    end
+  end
+
   def copy_params
     # Remove Rails internal parameters "action" and "controller" from URLs
     # They are always accessible via params["action"] and params["controller"]
@@ -38,4 +66,5 @@ module Orchid::ApplicationHelper
 
     return " class=\"site_#{section}\"".html_safe
   end
+
 end
