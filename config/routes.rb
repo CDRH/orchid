47acaaf6a7c05265e95cec57c777314dc9cd2ed4
Jessica Dussault <jdussault4@gmail.com>
2017-10-18 16:16:14 -0500

    tests checking for app routes before adding defaults

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,33 @@ Orchid::Engine.routes.draw do
 end
 
 Rails.application.routes.draw do
-  root 'general#index', as: :home
-  get 'about' => 'general#about', as: :about
 
-  # items
-  get 'browse' => 'items#browse', as: :browse
-  get 'browse/:facet' => 'items#browse_facet', as: :browse_facet, :constraints => { :facet => /[^\/]+/ }
-  get 'item/:id' => 'items#show', as: :item, :constraints => { :id => /[^\/]+/ }
-  get 'search' => 'items#index', as: :search
+  # generate a list of all the route names from the app running orchid
+  app_route_names = Rails.application.routes.routes.map { |r| r.name }
 
-  # errors
+  # define orchid default routes
+  default_routes = [
+    { get: 'about', location: 'general#about', as: :about, constraints: {} },
+    # items
+    { get: 'browse', location: 'items#browse', as: :browse, constraints: {} },
+    { get: 'browse/:facet', location: 'items#browse_facet', as: :browse_facet, constraints: { :facet => /[^\/]+/ } },
+    { get: 'item/:id', location: 'items#show', as: :item, constraints: { :id => /[^\/]+/ } },
+    { get: 'search', location: 'items#index', as: :search, constraints: {} }
+  ]
+
+  # if no route naming conflict between app and orchid, add orchid default route
+  default_routes.each do |route|
+    next if app_route_names.include?(route[:as].to_s)
+    get route[:get] => route[:location], as: route[:as], constraints: route[:constraints]
+  end
+
+  # use default if no home path specified
+  if !app_route_names.include?("home")
+    root 'general#index', as: "home"
+  end
+
+  # error handling
   match '/404', to: 'errors#not_found', via: :all
   match '/500', to: 'errors#server_error', via: :all
+
 end
