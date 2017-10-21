8eb84edb8d0098e6d7116258057eb279ec7cd1cd
Greg Tunink <techgique@gmail.com>
2017-10-20 19:45:44 -0500

    Add code to load main app extensions & overrides
    Based on: http://edgeguides.rubyonrails.org/engines.html#overriding-models-and-controllers
    but modified paths to locate override files in common Rails location

--- lib/orchid/engine.rb
+++ lib/orchid/engine.rb
@@ -1,5 +1,21 @@
 module Orchid
   class Engine < ::Rails::Engine
     isolate_namespace Orchid
+
+    config.to_prepare do
+      # Load main app's controller overrides
+      Dir.glob(Rails.root + "app/controllers/**/*_override.rb").each do |c|
+        # Files begin with `(ControllerName)Controller.class_eval do`
+        # and define or re-define methods of matching Orchid file
+        require_dependency(c)
+      end
+
+      # Load main app's model overrides
+      Dir.glob(Rails.root + "app/models/**/*_override.rb").each do |c|
+        # Files begin with `(ModelName).class_eval do`
+        # and define or re-define methods of matching Orchid file
+        require_dependency(c)
+      end
+    end
   end
 end
