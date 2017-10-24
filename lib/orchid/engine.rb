cf94e4bea0bc9da1126738f65bb3bcd836434e08
Greg Tunink <techgique@gmail.com>
2017-10-24 16:29:45 -0500

    Change path definitions to be portable

--- lib/orchid/engine.rb
+++ lib/orchid/engine.rb
@@ -4,14 +4,16 @@ module Orchid
 
     config.to_prepare do
       # Load main app's controller overrides
-      Dir.glob(Rails.root + "app/controllers/**/*_override.rb").each do |c|
+      Dir.glob(Rails.root.join("app", "controllers", "**",
+        "*_override.rb")).each do |c|
         # Files begin with `(ControllerName)Controller.class_eval do`
         # and define or re-define methods of matching Orchid file
         require_dependency(c)
       end
 
       # Load main app's model overrides
-      Dir.glob(Rails.root + "app/models/**/*_override.rb").each do |c|
+      Dir.glob(Rails.root.join("app", "models", "**",
+        "*_override.rb")).each do |c|
         # Files begin with `(ModelName).class_eval do`
         # and define or re-define methods of matching Orchid file
         require_dependency(c)
