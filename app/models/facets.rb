cdde2538bbdb6dd1a768a90ff41614190f4e8ce5
Jessica Dussault <jdussault4@gmail.com>
2018-07-19 09:22:39 -0500

    documentation for facets.rb placed in file

--- app/models/facets.rb
+++ app/models/facets.rb
@@ -1,3 +1,36 @@
+# ####################
+# Facets Configuration
+# ####################
+#
+# Individual facets must contain the following features:
+#   "[api_field]" => {
+#     "label" => "[label_to_display]",
+#     "display" => [boolean]
+# }
+#
+# The portions in brackets must be customized
+#   api_field: this should match a field in the API, and may be nested (creator.name)
+#   label_to_display: how the field appears in the website. you may use the same field
+#                     with multiple languages by changing the label
+#   display boolean: if true, this facet appears on the search and browse pages
+#                    if false, it only appears on the browse page
+#
+# ##################
+# Multiple Languages
+# ##################
+#
+# if the facets will need to pull different fields or display different labels
+# for multiple languages, you may organize them by language keys and customize behavior
+#
+# Facets.facet_info = {
+#   "en" => {
+#     "category" => { "label" => "Category", "display" : true }
+#   },
+#   "es" => {
+#     "category" => { "label" => "Categoría", "display": false }
+#   }
+# }
+
 module Facets
   extend Orchid::Facets
 
