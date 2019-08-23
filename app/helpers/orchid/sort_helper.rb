1322179a710311f1a7ee56d092d4e9dd97f15bbf
Greg Tunink <techgique@gmail.com>
2019-08-23 17:16:21 -0500

    Add section override checking to sort helpers

--- app/helpers/orchid/sort_helper.rb
+++ app/helpers/orchid/sort_helper.rb
@@ -28,44 +28,56 @@ module Orchid::SortHelper
   end
 
   def sort_fields_facets
-    # include "grouping" => "separator" to insert separator in dropdown
-    {
-      "count|desc" => t("search.sort.count_desc", default: "Count (most first)"),
-      "count|asc" => t("search.sort.count_asc", default: "Count (least first)"),
+    if @section.present? && respond_to?("#{@section}_sort_fields_facets")
+      send "#{@section}_sort_fields_facets"
+    else
+      # include "grouping" => "separator" to insert separator in dropdown
+      {
+        "count|desc" => t("search.sort.count_desc", default: "Count (most first)"),
+        "count|asc" => t("search.sort.count_asc", default: "Count (least first)"),
 
-      "terms" => "separator",
-      "term|asc" => t("search.sort.alpha_asc", default: "Alphabetically (A-Z)"),
-      "term|desc" => t("search.sort.alpha_desc", default: "Alphabetically (Z-A)")
-    }
+        "terms" => "separator",
+        "term|asc" => t("search.sort.alpha_asc", default: "Alphabetically (A-Z)"),
+        "term|desc" => t("search.sort.alpha_desc", default: "Alphabetically (Z-A)")
+      }
+    end
   end
 
   def sort_fields_search
-    if params["q"].present?
-      fields = {
-        "relevancy|desc" => t("search.sort.relevancy", default: "Relevancy"),
-        "rel_separator" => "separator"
-      }
+    if @section.present? && respond_to?("#{@section}_sort_fields_search")
+      send "#{@section}_sort_fields_search"
     else
-      fields = {}
-    end
+      if params["q"].present?
+        fields = {
+          "relevancy|desc" => t("search.sort.relevancy", default: "Relevancy"),
+          "rel_separator" => "separator"
+        }
+      else
+        fields = {}
+      end
 
-    fields.merge(sort_fields_search_additional)
+      fields.merge(sort_fields_search_additional)
+    end
   end
 
   def sort_fields_search_additional
-    # include "grouping" => "separator" to insert separator in dropdown
-    {
-      "date|asc" => t("search.sort.date_asc", default: "Date (earliest first)"),
-      "date|desc" => t("search.sort.date_desc", default: "Date (latest first)"),
+    if @section.present? && respond_to?("#{@section}_sort_fields_search_additional")
+      send "#{@section}_sort_fields_search_additional"
+    else
+      # include "grouping" => "separator" to insert separator in dropdown
+      {
+        "date|asc" => t("search.sort.date_asc", default: "Date (earliest first)"),
+        "date|desc" => t("search.sort.date_desc", default: "Date (latest first)"),
 
-      "titles" => "separator",
-      "title_sort|asc" => t("search.sort.title_asc", default: "Title (A-Z)"),
-      "title_sort|desc" => t("search.sort.title_desc", default: "Title (Z-A)"),
+        "titles" => "separator",
+        "title_sort|asc" => t("search.sort.title_asc", default: "Title (A-Z)"),
+        "title_sort|desc" => t("search.sort.title_desc", default: "Title (Z-A)"),
 
-      "creators" => "separator",
-      "creator.name|asc" => t("search.sort.creator_asc", default: "Creator (A-Z)"),
-      "creator.name|desc" => t("search.sort.creator_desc", default: "Creator (Z-A)")
-    }
+        "creators" => "separator",
+        "creator.name|asc" => t("search.sort.creator_asc", default: "Creator (A-Z)"),
+        "creator.name|desc" => t("search.sort.creator_desc", default: "Creator (Z-A)")
+      }
+    end
   end
 
   def sort_selected_label(sort_fields, sort_by)
