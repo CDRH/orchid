fe03bbcabc93adf32bce6957658aaac923b43d5c
Jessica Dussault <jdussault4@gmail.com>
2019-10-22 13:15:35 -0500

    renames field for clarity

--- app/helpers/orchid/display_helper.rb
+++ app/helpers/orchid/display_helper.rb
@@ -1,18 +1,18 @@
 module Orchid::DisplayHelper
   # res: API result hash
   # label: field display label ("Category")
-  # search_ele: the corresponding API field
+  # api_field: the corresponding API field
   # link: whether or not text or a link to search results
   #       for the given filter will be displayed, defaults
   #       to displaying a link
-  def metadata(res, label, search_ele, link: true)
-    data = res[search_ele]
+  def metadata(res, label, api_field, link: true)
+    data = res[api_field]
     if data.present?
       html = "<li><strong>#{label}:</strong> "
       dataArray = Array(data).map do |item|
         if link
-          search_params = { "f" => ["#{search_ele}|#{item}"] }
-          item_label = value_label(search_ele, item)
+          search_params = { "f" => ["#{api_field}|#{item}"] }
+          item_label = value_label(api_field, item)
           link_to item_label, prefix_path("search_path", search_params),
             rel: "nofollow"
         else
