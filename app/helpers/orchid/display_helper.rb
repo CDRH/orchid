b691c083b4737ee05413f35b0c9a41eb50db471d
Jessica Dussault <jdussault4@gmail.com>
2019-10-22 16:17:39 -0500

    major metadata helper overhaul
    moved <li> out of metadata method and into views
      makes it easier to override if called somewhere besides a <ul>, etc
      but this does mean checking if field is present? before using
    added support for nested fields
    split into multiple methods instead of a big'n
    pulled metadata_label into its own method for easier overriding
      also preemptively built "length" into method arguments for future work
    now search item response AND item show pages use same method for building metadata

--- app/helpers/orchid/display_helper.rb
+++ app/helpers/orchid/display_helper.rb
@@ -5,23 +5,54 @@ module Orchid::DisplayHelper
   # link: whether or not text or a link to search results
   #       for the given filter will be displayed, defaults
   #       to displaying a link
-  def metadata(res, label, api_field, link: true)
-    data = res[api_field]
+  # separator: the characters used to distinguish between items
+  #       in a list, defaults to " | "
+  def metadata(res, label, api_field, link: true, separator: " | ")
+    data = metadata_get_field_values(res, api_field)
+
     if data.present?
-      html = "<li><strong>#{label}:</strong> "
-      dataArray = Array(data).map do |item|
-        if link
-          search_params = { "f" => ["#{api_field}|#{item}"] }
-          item_label = value_label(api_field, item)
-          link_to item_label, prefix_path("search_path", search_params),
-            rel: "nofollow"
-        else
-          item
-        end
+      html = metadata_label(label, data.length)
+
+      # iterate through the field values
+      dataArray = data.map do |item|
+        link ? metadata_create_field_link(api_field, item) : item
       end
-      html += dataArray.join(" | ")
-      return html.html_safe
+      html += dataArray
+                .map { |i| "<span>#{i}</span>" }
+                .join(separator)
+
+      sanitize html
     end
+  end
+
+  # put together a search path with an f[] parameter
+  #   example: ?f[]=category|Writings
+  def metadata_create_field_link(api_field, item)
+    search_params = { "f" => ["#{api_field}|#{item}"] }
+    item_label = value_label(api_field, item)
+    link_to item_label, prefix_path("search_path", search_params),
+      rel: "nofollow"
+  end
+
+  # regardless of whether the results are a simple array or an array
+  # of hashes (a nested field), return a list of the values
+  def metadata_get_field_values(res, api_field)
+    # only nested fields use "." in their name
+    if api_field.include?(".")
+      nested = api_field.split(".")
+      # fields are only nested one level deep
+      res[nested.first].map { |doc| doc[nested[1]] }
+    else
+      Array(res[api_field])
+    end
+  end
+
+  # metadata label has been separated to allow apps to override included HTML
+  #   length is not currently used but is predicted to be used, or could
+  #   be used by overriding applications if desired
+  def metadata_label(label, length)
+    # TODO potentially add pluralization by locale
+    "<strong>#{label}:</strong> "
   end
 
 end
