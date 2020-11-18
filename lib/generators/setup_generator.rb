af6eaa1b79972a354001ac2701810608c2aaff71
Jessica Dussault <jdussault4@gmail.com>
2020-11-18 10:30:25 -0600

    renames "setup" generator to orchid_setup

--- lib/generators/setup_generator.rb
+++
@@ -1,9 +0,0 @@
-class SetupGenerator < Rails::Generators::Base
-
-  desc "Friendly warning that 'setup' is not longer supported"
-
-  def deprecation
-    puts "\nDid you mean `rails g orchid_setup` ?"
-  end
-
-end
