66ef9e210f58582c7059f50e98e695da4aeca4df
Jessica Dussault <jdussault4@gmail.com>
2020-11-18 15:42:48 -0600

    fixes typo

--- lib/generators/setup_generator.rb
+++ lib/generators/setup_generator.rb
@@ -1,6 +1,6 @@
 class SetupGenerator < Rails::Generators::Base
 
-  desc "Friendly warning that 'setup' is not longer supported"
+  desc "Friendly warning that 'setup' is no longer supported"
 
   def deprecation
     puts "\nDid you mean `rails g orchid_setup` ?"
