b90a3f4063352c2220aa12184bbb933799a9d601
Greg Tunink <techgique@gmail.com>
2021-08-25 15:32:54 -0500

    Fix removed Sprockets 4.x defaults
    Put link_tree ../images back with 4.x defaults as the two 3.x lines are
    the only difference from the 4.x default manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,8 +1,12 @@
 // Compatible with Sprockets 3.x defaults to avoid breaking existing apps
 // https://github.com/rails/sprockets/blob/master/UPGRADING.md#manifestjs
-//= link_tree ../images
 //= link application.css
 //= link application.js
+
+// Sprockets 4.x defaults
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
 
 // Orchid engine's manifest: orchid/app/assets/config/orchid/manifest.js
 //= link orchid/manifest.js
