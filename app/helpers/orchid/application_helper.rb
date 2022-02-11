ad247aae50c0f419787f76ee1dc625f4c2cc825c
William Dewey <wkdewey@gmail.com>
2022-02-11 11:03:53 -0600

    add button role to button links

--- app/helpers/orchid/application_helper.rb
+++ app/helpers/orchid/application_helper.rb
@@ -169,13 +169,15 @@ module Orchid::ApplicationHelper
         class: "btn btn-primary disabled",
         hreflang: lang_code,
         rel: "alternate",
-        tabindex: -1
+        tabindex: -1,
+        role: "button"
     else
       link_to lang_name,
         locale_link(lang_code),
         class: "btn btn-default",
         hreflang: lang_code,
-        rel: "alternate"
+        rel: "alternate",
+        role: "button"
     end
   end
 
