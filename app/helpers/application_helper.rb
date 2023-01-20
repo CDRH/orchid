9547a62427484c8c56412ffa7edd1d21538324fa
wkdewey <william.dewey@unl.edu>
2023-01-20 15:47:29 -0600

    add markdown parsing methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include Orchid::ApplicationHelper
+
+  def parse_md_brackets(query)
+    /\[(.*)\]/.match(query)[1] if /\[(.*)\]/.match(query)
+  end
+
+  def parse_md_parentheses(query)
+    /\]\((.*)\)/.match(query)[1] if /\]\((.*)\)/.match(query)
+  end
 end
