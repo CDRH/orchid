6597342bf2f1aec36ad464a0707e028e191fae6b
William Dewey <wdewey2@unl.edu>
2024-10-24 09:29:23 -0500

    change how facet_limit is set with sections to usual pattern

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -80,7 +80,7 @@ class ItemsController < ApplicationController
     @title = t "search.title"
     @res = @items_api.query(options)
     check_response
-    @facet_limit = @section.present? ? SECTIONS[@section]["facet_limit"] : PUBLIC["api_options"]["facet_limit"]
+    @facet_limit = @section.present? ? SECTIONS[@section]["api_options"]["facet_limit"] : PUBLIC["api_options"]["facet_limit"]
     render_overridable("items", "index")
   end
 
